ca0f06abd1de42783babc2ad33d6e94e88a68f1f
Inderpal Singh <inderpalsmutta@gmail.com>
2024-05-21 21:35:15 +0530

    Working

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,22 +1,48 @@
-import 'package:api_1_1_news/model/main_model.dart';
+
+import 'package:api_1_1_news/model/news_model.dart';
 import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
 
 class DetailPage extends StatelessWidget {
-  MainModel mainModel;
-  DetailPage({super.key, required this.mainModel});
+  NewsModel newsModel;
+  DetailPage({super.key, required this.newsModel});
 
   @override
   Widget build(BuildContext context) {
-    print('mainModel ${mainModel.articles!.length}');
+
+    // DateFormat dateFormat = DateFormat.yMMMMd();
+    
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: Text('ddd'),
-           
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Center(
+                  child: Image.network(newsModel.urlToImage.toString(),fit: BoxFit.cover,)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+              child: Column(
+                
+                children: [
+                  const SizedBox(height: 20),
+                  Text('${newsModel.title}', style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 20),
+                  Text('${newsModel.content}', style: const TextStyle(fontSize: 20)),
+                  const SizedBox(height: 20),
+                  Text('Source: ${newsModel.source!.name}', style: const TextStyle(fontSize: 20)),
+                  Text('Author: ${newsModel.author}', style: const TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+            
+          ],
         ),
+      ),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,91 +10,88 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(21)),
-                suffixIcon: const Icon(Icons.search_rounded),
-                hintText: 'Search news ...'),
-          ),
-          const SizedBox(height: 15),
-          FutureBuilder(
-              future: getAllNews(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-
-                if (snapshot.hasError) {
-                  // print('Error : $snapshot');
-                  return Center(child: Text('Error : ${snapshot.error}'));
-                }
-
-                if (snapshot.hasData) {
-                  MainModel allNews = snapshot.data!;
-
-                  return snapshot.data != null
-                      ? Expanded(
-                          child: ListView.builder(
-                              itemCount: allNews.articles!.length,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: SizedBox(
-                                    height: 200,
-                                    child: Card(
-                                      color: Colors.amberAccent.shade100,
-                                      child: Column(
-                                        children: [
-                                          ListTile(
-                                            leading: Image.network(allNews.articles![index].urlToImage.toString()),
-                                            title: Text(allNews.articles![index].title.toString()),
-                                            subtitle: Text(
-                                              allNews.articles![index].content.toString(),
-                                              maxLines: 2,
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border:  OutlineInputBorder(borderRadius: BorderRadius.circular(21)),
+                        suffixIcon: const Icon(Icons.search_rounded),
+                        hintText: 'Search here '),
+                  ),
+                ),
+               
+                Expanded(
+                  child: FutureBuilder(
+                      future: getAllNews(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        }
+                            
+                        if (snapshot.hasError) {
+                          // print('Error : $snapshot');
+                          return Center(child: Text('Error : ${snapshot.error}'));
+                        }
+                            
+                        if (snapshot.hasData) {
+                          MainModel allNews = snapshot.data!;
+                            
+                          return snapshot.data != null
+                              ? ListView.builder(
+                                  itemCount: allNews.articles!.length,
+                                  itemBuilder: (context, index) {
+                                    return SizedBox(
+                                      height: 200,
+                                      child: Card(
+                                        color: Colors.amberAccent.shade100,
+                                        child: Column(
+                                          children: [
+                                            ListTile(
+                                              leading: Image.network(allNews.articles![index].urlToImage.toString()),
+                                              title: Text(allNews.articles![index].title.toString(),maxLines: 2),
+                                              subtitle: Text(
+                                                allNews.articles![index].content.toString(),
+                                                maxLines: 2,
+                                              ),
                                             ),
-                                          ),
-                                          Container(
-                                            padding: const EdgeInsets.only(right: 20),
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.end,
-                                              children: [
-                                                ElevatedButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context, MaterialPageRoute(
-                                                        builder: (context)=> DetailPage(mainModel: allNews)));
-                                                    },
-                                                    child: const Text('Read More')),
-                                              ],
-                                            ),
-                                          )
-                                        ],
+                                            Container(
+                                              padding: const EdgeInsets.only(right: 20),
+                                              child: Row(
+                                                mainAxisAlignment: MainAxisAlignment.end,
+                                                children: [
+                                                  ElevatedButton(
+                                                      onPressed: () {
+                                                        Navigator.push(context, MaterialPageRoute(
+                                                            builder: (context)=> DetailPage(newsModel: allNews.articles![index])));
+                                                      },
+                                                      child: const Text('Read More')),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                );
-                              }),
-                        )
-                      : const Center(
-                          child: Text('No data'),
-                        );
-                }
-                return Container();
-              }),
-        ],
-      ),
-    ));
+                                    );
+                                  }): const Center(child: Text('No data'));
+                        }
+                        return Container();
+                      }),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 
   Future<MainModel?> getAllNews() async {
     MainModel? news;
-    const String baseUrl =
-        "https://newsapi.org/v2/everything?q=USA&apiKey=61e8994af44d470785c129a32a8851c1";
+    const String baseUrl = "https://newsapi.org/v2/everything?q=USA&apiKey=61e8994af44d470785c129a32a8851c1";
 
     http.Response response = await http.get(Uri.parse(baseUrl));
     if (response.statusCode == 200) {
