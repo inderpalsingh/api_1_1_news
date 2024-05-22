a7ec1ec61f5eb368c719f14b68a60d90806d843b
Inderpal Singh <inderpalsmutta@gmail.com>
2024-05-22 10:30:41 +0530

    Working all

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:api_1_1_news/screens/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:api_1_1_news/screens/detail_page.dart';
+
 import '../model/main_model.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String searchQueryUser = "USA" ;
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +30,27 @@ class HomePage extends StatelessWidget {
                 Container(
                   padding: const EdgeInsets.only(top: 20),
                   child: TextField(
+                    onTap: (){
+
+                      if(searchController.text.isNotEmpty){
+                        searchQueryUser = searchController.text;
+                      }
+
+                      setState(() {
+                        
+                      });
+                    },
+                    controller: searchController,
                     decoration: InputDecoration(
                         border:  OutlineInputBorder(borderRadius: BorderRadius.circular(21)),
                         suffixIcon: const Icon(Icons.search_rounded),
                         hintText: 'Search here '),
                   ),
                 ),
-               
+                const SizedBox(height: 20),
                 Expanded(
                   child: FutureBuilder(
-                      future: getAllNews(),
+                      future: getAllNews(searchQueryUser.toString()),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState == ConnectionState.waiting) {
                           return const Center(child: CircularProgressIndicator());
@@ -89,9 +111,10 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Future<MainModel?> getAllNews() async {
+  Future<MainModel?> getAllNews(String searchQuery) async {
     MainModel? news;
-    const String baseUrl = "https://newsapi.org/v2/everything?q=USA&apiKey=61e8994af44d470785c129a32a8851c1";
+    
+    String baseUrl = "https://newsapi.org/v2/everything?q=$searchQuery&apiKey=61e8994af44d470785c129a32a8851c1";
 
     http.Response response = await http.get(Uri.parse(baseUrl));
     if (response.statusCode == 200) {
